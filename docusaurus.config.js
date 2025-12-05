f7c413c6109bc39dd89e8e9b22b97003bd57ea18
Jon Jampen <dev@jonjampen.ch>
2025-12-05 10:35:57 +0100

    Adjusted edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -50,7 +50,7 @@ const config = {
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
                     editUrl:
-                        "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+                        "https://github.com/jonjampen/obsidian-xournalpp-docs/tree/main/",
                 },
                 blog: {
                     showReadingTime: true,
@@ -61,7 +61,7 @@ const config = {
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
                     editUrl:
-                        "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+                        "https://github.com/jonjampen/obsidian-xournalpp-docs/tree/main/",
                     // Useful options to enforce blogging best practices
                     onInlineTags: "warn",
                     onInlineAuthors: "warn",
